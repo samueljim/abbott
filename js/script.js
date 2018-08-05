d626862a20c65c37e1f1b645bf9fbec21992ccf8
samueljim <samueljim@msn.com>
2018-08-05 11:58:40 +1000

    added styles

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function loadChatbox() {
+    var e = document.getElementById("minim-chat");
+    e.style.display = "block";
+    var e = document.getElementById("maxi-chat");
+    e.style.display = "none";
+    var e = document.getElementById("chatbox");
+    e.style.margin = "0";
+}
+
+function closeChatbox() {
+    var e = document.getElementById("chatbox");
+    e.style.margin = "0 0 -1500px 0";
+}
+
+function minimChatbox() {
+    var e = document.getElementById("minim-chat");
+    e.style.display = "none";
+    var e = document.getElementById("maxi-chat");
+    e.style.display = "block";
+    var e = document.getElementById("chatbox");
+    e.style.margin = "0 0 -445px 0";
+}
